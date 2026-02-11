51d138940cb05fdcee0fd4a6c83457eda850d669
jwetter21 <108046115+jwetter21@users.noreply.github.com>
2026-02-11 07:21:21 -0800

    Commit of previous work

--- matrix.py
+++ matrix.py
@@ -1,13 +1,20 @@
 class Matrix:
     def __init__(self, row:int, col:int):
         self.__matrix = [[0]*row]*col
-            
+        self.__row = row
+        self.__col = col            
         # self.__isVector = False
         # self.__isSquare = False
     def __init__(self, vector:list[int]):
-        pass
+        self.__matrix = [vector]
+        self.__row = 1
+        self.__col = len(vector)
+
     def __init__(self, matrix:list[list[int]]):
-        pass
+        self.__matrix = matrix
+        self.__row = len(matrix)
+        self.__col = len(matrix[0])
+
     def col(self):
         pass
     def row(self):
