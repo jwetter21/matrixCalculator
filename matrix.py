2394e122ecb17c3e631b3bd86ece514381a448f8
Jasmine Wetter Hiebert <108046115+jwetter21@users.noreply.github.com>
2026-02-11 21:40:43 -0800

    added some commenting to Matrix

--- matrix.py
+++ matrix.py
@@ -1,14 +1,17 @@
+
 class Matrix:
     def __init__(self, row:int, col:int):
+        # Instantiate 
         self.__matrix = [[0]*row]*col
         self.__row = row
         self.__col = col            
         # self.__isVector = False
         # self.__isSquare = False
     def __init__(self, vector:list[int]):
+        # I am choosing to view vectors as having one column and many rows.
         self.__matrix = [vector]
-        self.__row = 1
-        self.__col = len(vector)
+        self.__row = len(vector)
+        self.__col = 1
 
     def __init__(self, matrix:list[list[int]]):
         self.__matrix = matrix
@@ -23,7 +26,10 @@ class Matrix:
         pass
     def cross(self, other):
         pass
-    def resize(self, row, col):
+    # def resize(self, row, col):
+    #     I might choose not to allow resizing
+    #     pass
+    def reset(self, matrix:list[list[int]]):
         pass
     def add(self, other):
         pass
