aedbcce975edf6a641158ef4492678f94c0dafe5
Jasmine Wetter Hiebert <108046115+jwetter21@users.noreply.github.com>
2026-03-03 06:49:18 -0800

    created test class for Matrix and the test method test_init to ensure that Matrix intiates as expected

--- matrixTest.py
+++ matrixTest.py
@@ -1,7 +1,13 @@
 import unittest
+from matrix import Matrix
+from fraction import Fraction
 
-class TestMatrix(unittest.TextCase):
-    pass
+class TestMatrix(unittest.TestCase):
+    def test_init(self):
+        A = Matrix([[3,-13/12, 6.5],[33/4, '1 1/7', -1.2],['-88/90', 0.99, 5]])
+
+        self.assertEqual(A.get_matrix[0][1], Fraction(-13/12))
+        self.assertEqual(A.get_matrix[2][0], Fraction('88/90'))
 
 if __name__ == '__main__':
     unittest.main()
