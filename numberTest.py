c08a058503b6d694e914d5ed1447d3a4f58896d3
Jasmine Wetter Hiebert <108046115+jwetter21@users.noreply.github.com>
2026-03-03 06:52:03 -0800

    Changed the requirements for some tests since the implementation of Number changed

--- numberTest.py
+++ numberTest.py
@@ -10,7 +10,7 @@ class TestNumber(unittest.TestCase):
         x = Number("1 1/2")
         y = Number("13/3") 
         z = x+y
-        self.assertEqual(z, Fraction("35/6"))
+        self.assertEqual(z, Fraction("35/6")) # FIXME: I don't know if I want addition to return a Fraction object
         # self.assertIsInstance(z, Fraction)
         # 13/3 = 4 1/3
         # 1.5 + 4.333 = 5.333
